ee57a58f43508f859fd75332dc7771ff19240a1c
Kshitiz Shrestha <er.kshitizshrestha@gmail.com>
2021-03-20 01:26:08 +0545

    navigation bar

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,14 +1,46 @@
 <template>
     <v-app>
-        <v-navigation-drawer app>
-        </v-navigation-drawer>
+    <v-navigation-drawer app>
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="title">
+            Application
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            subtext
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-list
+        dense
+        nav
+      >
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          link
+          :to = "item.route"
+          router
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
 
         <v-app-bar app>
         </v-app-bar>
 
         <v-main>
             <v-container fluid>
-                <router-view></router-view>
+                <router-view>
+                </router-view>
             </v-container>
         </v-main>
         <v-footer app>
@@ -18,6 +50,21 @@
 
 <script>
     export default {
-
+        data () {
+        return {
+            items: [
+            { title: 'Dashboard', icon: 'mdi-view-dashboard' },
+            { title: 'Login', icon: 'mdi-image' , route: '/login' },
+            { title: 'Register', icon: 'mdi-help-box', route: '/register' },
+            ],
+            right: null,
+        }
+        },
     }
 </script>
+
+<style scoped>
+    .v-list-item {
+        text-decoration: none;
+    }
+</style>
