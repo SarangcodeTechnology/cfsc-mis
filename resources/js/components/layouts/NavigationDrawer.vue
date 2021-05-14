7e0835940eb8a3f60a50ceb40d693fda65a0fe8b
Ajeeb Rimal <er.ajeebrimal@gmail.com>
2021-05-14 21:23:23 +0545

    navigation drawer fixed

--- resources/js/components/layouts/NavigationDrawer.vue
+++ resources/js/components/layouts/NavigationDrawer.vue
@@ -1,105 +1,115 @@
 <template>
-  <v-list dense nav>
-    <div v-for="(item, i) in items" :key="i">
-      <v-list-item
-        v-if="!item.subItems && checkPermission(item.check)"
-        :to="item.route"
-        link
-        router
-        color="white"
-      >
-        <v-list-item-icon>
-          <v-icon>{{ item.icon }}</v-icon>
-        </v-list-item-icon>
-        <v-list-item-content>
-          <v-list-item-title class="pt-1"
-            ><strong>{{ item.title }}</strong></v-list-item-title
-          >
-        </v-list-item-content>
-      </v-list-item>
-      <v-list-group v-if="item.subItems && checkPermissionForSubItems(item.subItems)" :prepend-icon="item.icon" color="grey darken-4">
-        <template v-slot:activator>
-          <v-list-item-content>
-            <v-list-item-title class="pt-1"
-              ><strong>{{ item.title }}</strong></v-list-item-title
+    <v-list dense nav>
+        <div v-for="(item, i) in items" :key="i">
+            <v-list-item
+                v-if="!item.subItems && checkPermission(item.check)"
+                :to="item.route"
+                color="white"
+                link
+                router
             >
-          </v-list-item-content>
-        </template>
-        <v-list-item
-          v-for="(subItem, j) in item.subItems"
-          :key="j"
-          :to="subItem.route"
-          v-if="checkPermission(subItem.check)"
-          link
-          router
-          color="white"
-        >
-          <v-list-item-icon>
-            <v-icon>{{ subItem.icon }}</v-icon>
-          </v-list-item-icon>
-          <v-list-item-content>
-            <v-list-item-title class="pt-1"
-              ><strong>{{ subItem.title }}</strong></v-list-item-title
-            >
-          </v-list-item-content>
-        </v-list-item>
-      </v-list-group>
-      <v-divider class="ma-0 pa-0"></v-divider>
-    </div>
-  </v-list>
+                <v-list-item-icon>
+                    <v-icon>{{ item.icon }}</v-icon>
+                </v-list-item-icon>
+                <v-list-item-content>
+                    <v-list-item-title class="pt-1"
+                    ><strong>{{ item.title }}</strong></v-list-item-title
+                    >
+                </v-list-item-content>
+            </v-list-item>
+            <v-list-group v-if="item.subItems && checkPermissionForSubItems(item.subItems)" :prepend-icon="item.icon"
+                          color="grey darken-4">
+                <template v-slot:activator>
+                    <v-list-item-content>
+                        <v-list-item-title class="pt-1"
+                        ><strong>{{ item.title }}</strong></v-list-item-title
+                        >
+                    </v-list-item-content>
+                </template>
+                <v-list-item
+                    v-for="(subItem, j) in item.subItems"
+                    v-if="checkPermission(subItem.check)"
+                    :key="j"
+                    :to="subItem.route"
+                    color="white"
+                    link
+                    router
+                >
+                    <v-list-item-icon>
+                        <v-icon>{{ subItem.icon }}</v-icon>
+                    </v-list-item-icon>
+                    <v-list-item-content>
+                        <v-list-item-title class="pt-1"
+                        ><strong>{{ subItem.title }}</strong></v-list-item-title
+                        >
+                    </v-list-item-content>
+                </v-list-item>
+            </v-list-group>
+            <v-divider class="ma-0 pa-0"></v-divider>
+        </div>
+    </v-list>
 </template>
 
 <script>
-import { mapState } from "vuex";
+import {mapState} from "vuex";
 
 export default {
-  data() {
-    return {
-      miniVariant: false,
-      items: [
-        { title: "ड्यसबोर्ड", icon: "mdi-view-dashboard", route: "/dashboard", check: "can_browse_dashboard"},
-        { title: "गृहपृष्ठ", icon: "mdi-home", route: "/home", check: "can_browse_home" },
-        { title: "खाताहरु", icon: "mdi-account-circle", route: "/users" , subItems:[
-            { title: "प्रयोगकर्ताहरू", icon: "mdi-account-circle", route: "/users" , check: "can_browse_users"},
-            { title: "भूमिकाहरू", icon: "mdi-account-settings", route: "/roles" , check: "can_browse_roles"},
-            { title: "अनुमतिहरू", icon: "mdi-key", route: "/permissions", check: "can_browse_permissions" }
-        ]
+    data() {
+        return {
+            miniVariant: false,
+            items: [
+                {title: "ड्यसबोर्ड", icon: "mdi-view-dashboard", route: "/dashboard", check: "can_browse_dashboard"},
+                {title: "गृहपृष्ठ", icon: "mdi-home", route: "/home", check: "can_browse_home"},
+                {
+                    title: "खाताहरु", icon: "mdi-account-circle", route: "/users", subItems: [
+                        {
+                            title: "प्रयोगकर्ताहरू",
+                            icon: "mdi-account-circle",
+                            route: "/users",
+                            check: "can_browse_users"
+                        },
+                        {title: "भूमिकाहरू", icon: "mdi-account-settings", route: "/roles", check: "can_browse_roles"},
+                        {title: "अनुमतिहरू", icon: "mdi-key", route: "/permissions", check: "can_browse_permissions"}
+                    ]
+                },
+                {title: "सामुदायिक वन थप", icon: "mdi-plus", route: "/cf-data-edit", check: "can_browse_cfdata"},
+                {title: "सामुदायिक वन विवरण", icon: "mdi-border-all", route: "/cf-data", check: "can_browse_cfdata"},
+            ],
+        };
+    },
+    computed: {
+        ...mapState({
+            userPermissions: (state) => state.webservice.resources.userPermissions
+        })
+    },
+    methods: {
+        setMiniVariant() {
+            var tempthis = this;
+            this.miniVariant = !this.miniVariant;
+            this.$store.dispatch("setMiniVariant", tempthis.miniVariant);
         },
-        { title: "सामुदायिक वन थप", icon: "mdi-plus", route: "/cf-data-edit", check: "can_browse_cfdata" },
-        { title: "सामुदायिक वन विवरण", icon: "mdi-border-all", route: "/cf-data", check: "can_browse_cfdata" },
-      ],
-    };
-  },
-  computed:{
-      ...mapState({
-        userPermissions: (state) => state.webservice.resources.userPermissions
-      })
-  },
-  methods: {
-    setMiniVariant() {
-      var tempthis = this;
-      this.miniVariant = !this.miniVariant;
-      this.$store.dispatch("setMiniVariant", tempthis.miniVariant);
-    },
-    checkPermission(check) {
-      return this.userPermissions.includes(check);
-    },
-    checkPermissionForSubItems(subItems){
-      return true;
-      let tempthis = this;
-      subItems.forEach(function(item){
-          if(!tempthis.userPermissions.includes(item.check)){
+        checkPermission(check) {
+            return this.userPermissions.includes(check);
+        },
+        checkPermissionForSubItems(subItems) {
+            var count = 0;
+            let tempthis = this;
+            subItems.forEach(function (item) {
+                if (tempthis.checkPermission(item.check)) {
+                    count++;
+                }
+            });
+            if (count === 0) {
+                return false;
+            }
             return true;
-          }
-      });
-      return false;
-    }
-  },
+        }
+    },
 };
 </script>
 
 <style scoped>
 .v-list-item {
-  text-decoration: none;
+    text-decoration: none;
 }
 </style>
